c4262ec67f5851cd5ca9f17d4eebe23572f7e677
Bhavuk Jain <bhavuk_92@yahoo.co.in>
2018-01-30 16:33:21 +0530

    APIs added.

---
+++ src/api.js
@@ -0,0 +1,57 @@
+
+const api = "http://localhost:3001"
+
+
+// Generate a unique token for storing your bookshelf data on the backend server.
+let token = localStorage.token
+if (!token)
+  token = localStorage.token = Math.random().toString(36).substr(-8)
+
+const headers = {
+  'Accept': 'application/json',
+  'Authorization': token
+}
+
+export const getCategories = () =>
+  fetch(`${api}/categories`, { headers })
+    .then(res => res.json())
+    .then(data => data.categories)
+
+
+export const getPosts = () =>
+  fetch(`${api}/posts`, { headers })
+    .then(res => res.json())
+    .then(data => data)
+
+
+export const createPost = (post) =>
+  fetch(`${api}/posts`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(post)
+  }).then(res => res.json())
+    .then(data => data)
+
+
+    export const updateVote = (postid,type) =>
+  fetch(`${api}/posts/${postid}`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({option:type})
+  }).then(res => res.json())
+    .then(data => data)
+
+
+
+
+// export const getAllPosts = () =>
+//   fetch(`${api}/books/${bookId}`, { headers })
+//     .then(res => res.json())
+//     .then(data => data.posts)
+
